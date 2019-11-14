dcffd134746da2f95cdb11016a23c0dee8a59d84
cdanismaz <cansu.danismaz@gmail.com>
2019-11-14 10:33:26 +0300

    credentials deleted

--- src/test/java/com/cd/automationtemplate/config/Constants.java
+++ src/test/java/com/cd/automationtemplate/config/Constants.java
@@ -3,13 +3,13 @@ package com.cd.automationtemplate.config;
 public class Constants {
 
     public static class Urls {
-        public static final String LOGIN_PAGE = "http://dlpdevelop.vcerf.net";
-        public static final String DASHBOARD_PAGE = "http://dlpdevelop.vcerf.net/dashboard/dashboard";
+        public static final String LOGIN_PAGE = "..";
+        public static final String DASHBOARD_PAGE = "..";
     }
 
     public static class Credentials {
-        public static final String VALID_USERNAME = "admin";
-        public static final String VALID_PASSWORD = "admin";
+        public static final String VALID_USERNAME = "..";
+        public static final String VALID_PASSWORD = "..";
 
         public static final String INVALID_USERNAME = "test";
         public static final String INVALID_PASSWORD = "test";
